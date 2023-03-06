6317653b2b3f1828e566245d0d87fd319bd29c5d
kanishk062001 <kanishk112001@gmail.com>
2023-03-06 07:44:21 +0530

    new file

---
+++ c++ for competitve/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    cout<< "enter input";
+    int a;
+    cin>>a;
+    cout<<" this is "<<a;
+
+    for (const string& word : msg)
+    {
+        cout << word << " \n";
+    }
+    cout << endl;
+}
